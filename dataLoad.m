31f1b02d208332491d71af7379510e955a92423f
ヾ(≧∇≦*)ゝ <iluvmizuki@gmail.com>
2016-10-12 21:20:33 +0200

    :3

--- dataLoad.m
+++ dataLoad.m
@@ -1,7 +1,29 @@
 function data = dataLoad(filename)
+pwd
+cd('C:\Users\Kristofer Pedersen\Documents\MATLAB\GitHub\Bacteria-Data-Analysis\Bacteria');
+
+table = readtable(filename);
+table=[table];
+
+index = zeros(height(table),1);
 
 
+% remove temperetures outside of range
 
+tindex = table{:,1}>60 | table{:,1}<10;
 
+table(tindex,:)
+
+% remove invalid Growth rate
+
+gindex = table{:,2}<0;
+
+table(gindex,:)
+
+% remove invalid bacteria type
+
+bindex = table{:,3}>4 | table{:,3}<1;
+
+table(bindex,:)
 
 end
