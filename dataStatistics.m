72148052ba62a7404945dcdf101a5353ac743791
TadeaVeng <TadeaVeng@gmail.com>
2016-10-27 13:40:46 +0200

    Data statistik

---
+++ dataStatistics.m
@@ -0,0 +1,44 @@
+% Data statistics function 
+
+function result = dataStatistics(data,statistic);
+
+D=readtable(data);
+
+
+% Den relevante statestik vælges
+switch statistic
+    case 'Mean Temperature'
+        
+        
+    case 'Mean Growth rate'
+        
+        
+    case 'Std Temperature'
+        
+        
+    case 'Std Growth rate'
+        
+        
+    case 'Rows' 
+        
+        
+    case 'Mean Cold Growth rate'
+        
+        
+    case 'Mean Hot Growth rate'
+        
+
+% Afslut switch
+end
+
+        
+
+% Afslut funktion
+end
+
+
+
+
+
+
+
